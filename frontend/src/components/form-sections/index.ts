cc803025cbeccd6d7ee3937fb4134be3df074462
erusek1 <erik@erikrusekelectric.com>
2025-03-17 17:35:27 -0400

    Add form-sections index file

---
+++ frontend/src/components/form-sections/index.ts
@@ -0,0 +1,11 @@
+// frontend/src/components/form-sections/index.ts
+
+import ElectricalSection from './ElectricalSection';
+
+// Export form sections
+export {
+  ElectricalSection
+};
+
+// Re-export types
+export * from './FormSectionTypes';
